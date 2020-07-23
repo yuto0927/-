38c1d443cda823c9a09ca23a167578d8a9813286
yuto <yuto.ogami5@gmail.com>
2020-07-23 05:55:46 +0000

    kadai

---
+++ kadai3.php
@@ -0,0 +1,5 @@
+<?php
+$hello = "Hello";
+$name = "yuto";
+$world = "'s World!";
+echo $hello . $name . $world;
---
+++ kadai4.php
@@ -0,0 +1,4 @@
+<?php
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
